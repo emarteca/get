e1e3f0ab4722e1dc45af4090132bed0412b2804c
Ellen Arteca <ellen.arteca@gmail.com>
2020-04-26 20:38:19 -0400

    setting up testing infrastructure

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,16 @@ const path = require('path');
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['node_modules', path.resolve(__dirname, 'lib')]
+  testPathIgnorePatterns: ['node_modules', path.resolve(__dirname, 'lib')],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: '.',
+        outputName: 'junit-unit-tests.xml',
+      },
+    ],
+  ],
+  setupFiles: ['<rootDir>/unit-error-hack.js']
 };
